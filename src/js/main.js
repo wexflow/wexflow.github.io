1ebe9c3492a31cca54b0eb678356a4cbb76773b3
aelassas <akram.elassas@gmail.com>
2025-07-07 16:55:33 +0100

    feat: add github star button

--- src/js/main.js
+++ src/js/main.js
@@ -4,16 +4,20 @@ import {
   initHeaderScroll,
   initLanguageMenu,
   initThemeToggle,
+  loadGitHubButtonsScript,
+  initGitHubButton,
   updateFooterYear
 } from './ui.js'
 
 window.addEventListener('DOMContentLoaded', async () => {
   try {
+    await loadGitHubButtonsScript()
     await loadInitialLanguage()
     initHamburgerMenu()
     initHeaderScroll()
     initLanguageMenu(setLang)
     initThemeToggle()
+    initGitHubButton()
     updateFooterYear()
   } catch (err) {
     console.error('Error during app initialization:', err)
